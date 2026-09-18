79dad949d062e9bbbe5a1d6e1eb81c17ea6574fe
finoana <finoana@rakotoson.com>
2026-09-18 14:21:00 +0300

    feat: Séparation des nombres négatifs et positifs

---
+++ NegatifPositif.js
@@ -0,0 +1,27 @@
+function NegatifPositif(T) {
+    let gauche = 0;
+    let droite = T.length - 1;
+
+    while (gauche < droite) {
+
+        while (gauche < droite && T[gauche] <= 0) {
+            gauche++;
+        }
+
+        while (gauche < droite && T[droite] > 0) {
+            droite--;
+        }
+
+        if (gauche < droite) {
+            let temporaire = T[gauche];
+            T[gauche] = T[droite];
+            T[droite] = temporaire;
+
+            gauche++;
+            droite--;
+        }
+    }
+
+    return T;
+}
+
